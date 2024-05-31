7de4b9910482e38f4f42daeddfee7715b9b185fd
BS-JeremyB <jeremy.bazin@bstorm.be>
2024-05-31 15:20:08 +0200

    moq

--- TI_Devops_2024.TEST/UnitTest1.cs
+++ TI_Devops_2024.TEST/UnitTest1.cs
@@ -10,6 +10,7 @@ namespace BookServiceTests
     public class BookServiceTests
     {
 
+
         private readonly Mock<IBookRepository> _bookRepositoryMock;
         private readonly BookService _bookService;
 
@@ -20,19 +21,107 @@ namespace BookServiceTests
         }
 
         [Fact]
-        public void GetAll_Retourne_Livres()
+        public void GetAll_ShouldReturnAllBooks()
         {
-            List<Book> books = new List<Book>
-            {
-                new Book { Title = "Test", ISBN = "99999999999" },
-                new Book { Title = "Test2", ISBN = "88888888888" },
-            };
+            // Arrange
+            var books = new List<Book>
+                {
+                    new Book { ISBN = "978-3-16-148410-0", Title = "Test Book 1" },
+                    new Book { ISBN = "978-1-56619-909-4", Title = "Test Book 2" }
+                };
 
             _bookRepositoryMock.Setup(repo => repo.GetAll()).Returns(books);
 
-            IEnumerable<Book> results = _bookService.GetAll();
+            // Act
+            var result = _bookService.GetAll();
 
-            Assert.Equal(books, results);
+            // Assert
+            Assert.Equal(books, result);
+        }
+
+        [Fact]
+        public void GetByISBN_ShouldReturnBook_WhenBookExists()
+        {
+            // Arrange
+            var book = new Book { ISBN = "978-3-16-148410-0", Title = "Test Book" };
+            _bookRepositoryMock.Setup(repo => repo.GetById("978-3-16-148410-0")).Returns(book);
+
+            // Act
+            var result = _bookService.GetByISBN("978-3-16-148410-0");
+
+            // Assert
+            Assert.Equal(book, result);
+        }
+
+        [Fact]
+        public void GetByISBN_ShouldThrowException_WhenBookDoesNotExist()
+        {
+            // Arrange
+            _bookRepositoryMock.Setup(repo => repo.GetById("978-3-16-148410-0")).Returns((Book)null);
+
+            // Act & Assert
+            Assert.Throws<KeyNotFoundException>(() => _bookService.GetByISBN("978-3-16-148410-0"));
+        }
+
+        [Fact]
+        public void Create_ShouldThrowException_WhenBookAlreadyExists()
+        {
+            // Arrange
+            var book = new Book { ISBN = "978-3-16-148410-0", Title = "Test Book" };
+            _bookRepositoryMock.Setup(repo => repo.ExistByUnicityCriteria(book)).Returns(true);
+
+            // Act & Assert
+            Assert.Throws<Exception>(() => _bookService.Create(book));
+        }
+
+        [Fact]
+        public void Create_ShouldReturnISBN_WhenBookIsCreated()
+        {
+            // Arrange
+            var book = new Book { ISBN = "978-3-16-148410-0", Title = "Test Book" };
+            _bookRepositoryMock.Setup(repo => repo.ExistByUnicityCriteria(book)).Returns(false);
+            _bookRepositoryMock.Setup(repo => repo.Create(book)).Returns(book.ISBN);
+
+            // Act
+            var result = _bookService.Create(book);
+
+            // Assert
+            Assert.Equal(book.ISBN, result);
+        }
+
+        [Fact]
+        public void Update_ShouldThrowException_WhenBookDoesNotExist()
+        {
+            // Arrange
+            var book = new Book { ISBN = "978-3-16-148410-0", Title = "Test Book" };
+            _bookRepositoryMock.Setup(repo => repo.ExistByISBN("978-3-16-148410-0")).Returns(false);
+
+            // Act & Assert
+            Assert.Throws<KeyNotFoundException>(() => _bookService.Update("978-3-16-148410-0", book));
+        }
+
+        [Fact]
+        public void Delete_ShouldThrowException_WhenBookDoesNotExist()
+        {
+            // Arrange
+            _bookRepositoryMock.Setup(repo => repo.ExistByISBN("978-3-16-148410-0")).Returns(false);
+
+            // Act & Assert
+            Assert.Throws<KeyNotFoundException>(() => _bookService.Delete("978-3-16-148410-0"));
+        }
+
+        [Fact]
+        public void Delete_ShouldReturnTrue_WhenBookIsDeleted()
+        {
+            // Arrange
+            _bookRepositoryMock.Setup(repo => repo.ExistByISBN("978-3-16-148410-0")).Returns(true);
+            _bookRepositoryMock.Setup(repo => repo.Delete("978-3-16-148410-0")).Returns(true);
+
+            // Act
+            var result = _bookService.Delete("978-3-16-148410-0");
+
+            // Assert
+            Assert.True(result);
         }
     }
 }
